4b8895cad6564201ce93b46d2bee97bd775216b2
anuragchvn-blip <118217089+anuragchvn-blip@users.noreply.github.com>
2024-04-25 03:39:46 +0530

    main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const jobs = [
   {
     title: "Software Engineer",
-    image: "images/software-engineer.svg",
+    image: "software-engineer.svg",
     details:
       "Responsible for designing, developing and maintaining software systems and applications.",
     openPositions: "2",
@@ -10,7 +10,7 @@ const jobs = [
 
   {
     title: "Data Scientist",
-    image: "images/data-scientist.svg",
+    image: "data-scientist.svg",
     details:
       "Responsible for collecting, analyzing and interpreting large data sets to help organizations make better decisions.",
     openPositions: "3",
@@ -19,7 +19,7 @@ const jobs = [
 
   {
     title: "Project Manager",
-    image: "images/project-manager.svg",
+    image: "project-manager.svg",
     details:
       "Responsible for planning, executing and closing projects on time and within budget.",
     openPositions: "1",
@@ -28,7 +28,7 @@ const jobs = [
 
   {
     title: "Product Manager",
-    image: "images/product-manager.svg",
+    image: "product-manager.svg",
     details:
       "Responsible for managing the entire product life cycle, from ideation to launch and post-launch maintenance.",
     openPositions: "1",
@@ -37,7 +37,7 @@ const jobs = [
 
   {
     title: "Sales Representative",
-    image: "images/sales-representative.svg",
+    image: "sales-representative.svg",
     details:
       "Responsible for reaching out to potential customers and closing sales deals.",
     openPositions: "4",
@@ -46,7 +46,7 @@ const jobs = [
 
   {
     title: "Marketing Manager",
-    image: "images/marketing-manager.svg",
+    image: "marketing-manager.svg",
     details:
       "Responsible for creating and executing marketing strategies to promote a company or product.",
     openPositions: "1",
